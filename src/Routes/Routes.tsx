2adfc008c44db03c2fb40b98b32ecbfd3a6bf4c2
farhana988 <farhana744255@gmail.com>
2025-10-23 19:51:52 +0600

    changed folder structure

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -1,22 +1,17 @@
+import MainLayout from "@/components/Layout/MainLayout";
+import Home from "@/pages/Home";
 import { createBrowserRouter } from "react-router";
-import MainLayout from "../Layout/MainLayout";
-import ErrorPage from "../pages/ErrorPage";
-import Home from "../pages/Home";
+
 
 const router = createBrowserRouter([
-  {
+ {
+    Component: MainLayout,
     path: "/",
-    element: <MainLayout />,
-    errorElement: <ErrorPage />,
     children: [
       {
+        Component: Home,
         path: "/",
-        element: <Home />,
       },
-      // {
-      //  path: "/",
-      //  element:
-      // }
     ],
   },
 ]);
